4cdf2459e8781012e677e8c1acd5ca8bbdb2dc95
janav-007 <168264262+janav-007@users.noreply.github.com>
2024-06-17 12:31:32 +0530

    Time: 627 ms (17.28%) | Memory: 18.3 MB (96.12%) - LeetSync

--- 575-distribute-candies/distribute-candies.py
+++ 575-distribute-candies/distribute-candies.py
@@ -1,9 +1,14 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
+        n = len(candyType)//2
         candy_hash = {}
         for candy in candyType:
             if candy not in candy_hash:
                 candy_hash[candy] = 1
             else:
                 candy_hash[candy] += 1
-        return min(len(candy_hash),len(candyType)//2)
+        x = len(candy_hash)
+        if x<=n:
+            return x
+        else:
+            return n
